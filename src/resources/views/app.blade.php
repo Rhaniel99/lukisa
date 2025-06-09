954f704ade6acf527dc88fd35d08b333168e0edb
rhaniel99 <rhanielmonteiro.13@gmail.com>
2025-06-09 16:43:53 -0400

    configuracao de pastas pro front-end definida

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title inertia>{{ config('app.name', 'AEMINA') }}</title>
+        <title inertia>{{ config('app.name', 'Lukisa') }}</title>
         <!-- Scripts -->
         @routes
         @viteReactRefresh
