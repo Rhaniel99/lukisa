f20ee6ddfb8417b56bb92a338354a1e54e129b3f
rhaniel99 <rhanielmonteiro.13@gmail.com>
2025-06-18 09:21:54 -0400

    erro durante o compose

--- src/modules/Authentication/app/Services/AuthenticationService.php
+++ src/modules/Authentication/app/Services/AuthenticationService.php
@@ -2,6 +2,8 @@
 
 namespace Modules\Authentication\Services;
 
+use Modules\Authentication\Interfaces\Services\IAuthenticationService;
+
 class AuthenticationService implements IAuthenticationService {
     public function __construct()
     {}
