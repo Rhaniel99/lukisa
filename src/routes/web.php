a78797d2f7b07c5dde414d1a96e041eb7277d321
rhaniel99 <rhanielmonteiro.13@gmail.com>
2025-07-02 11:34:15 -0400

    adicionado o horizon

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,16 +1,22 @@
 <?php
 
+use App\Models\User;
+use App\Notifications\MessageTestNotification;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => inertia('Public/Home'))->name('home');
 
 Route::get('send', function () {
     $message['status'] = request()->query('status', 'success');
-    $message['body'] = 'Mensagem padrao notificação...';
+    $message['body'] = 'Esta é uma mensagem de teste para todos os usuários!';
 
-    $u = \App\Models\User::first();
+    // 1. Pega todos os usuários
+    $users = User::all();
 
-    $u->notify(new \App\Notifications\MessageTestNotification($message));
+    // 2. Envia a notificação para cada um deles
+    foreach ($users as $user) {
+        $user->notify(new MessageTestNotification($message));
+    }
 
-    return 'Notificacao enviada';
+    return 'Notificação enviada para todos os usuários.';
 });
