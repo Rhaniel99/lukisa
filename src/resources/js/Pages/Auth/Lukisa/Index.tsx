fe94cfb3f35893e645c921e0c92e66f7b786b96e
rhaniel99 <rhanielmonteiro.13@gmail.com>
2025-07-02 09:47:04 -0400

    erro no reverb

--- src/resources/js/Pages/Auth/Lukisa/Index.tsx
+++ src/resources/js/Pages/Auth/Lukisa/Index.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, Head, usePage } from "@inertiajs/react";
 import { Button } from "@/components/ui/button";
+import { PageProps } from "@/Types/models";
 
 const Index: React.FC = () => {
-    const { auth } = usePage().props;
+    const { auth } = usePage<PageProps>().props;
+
+    useEffect(() => {
+        // Escuta no canal 'canal-de-teste' pelo evento '.evento.teste'
+        const channel = window.Echo.channel("canal-de-teste");
+
+        channel.listen(".evento.teste", (data: { message: string }) => {
+            console.log("Evento recebido do Reverb:", data);
+            alert(data.message);
+        });
+
+        // Função de limpeza: para de escutar quando o componente é desmontado
+        return () => {
+            channel.stopListening(".evento.teste");
+            window.Echo.leave("canal-de-teste");
+        };
+    }, []); // O array vazio garante que o efeito rode apenas uma vez
 
     return (
         <>
@@ -14,7 +31,8 @@ const Index: React.FC = () => {
                 <div className="text-center">
                     {/* Mensagem de boas-vindas */}
                     <h1 className="mb-8 text-4xl font-bold text-gray-800 dark:text-gray-200">
-                        Bem-vindo usuário autenticado ao Lukisa: {auth.user.name}
+                        Bem-vindo usuário autenticado ao Lukisa:{" "}
+                        {auth.user.name}
                     </h1>
 
                     <Button>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Events\TesteReverbEvent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => inertia('Public/Home'))->name('home');
 
+Route::get('/teste-reverb', function () {
+    broadcast(new TesteReverbEvent());
+    return 'Evento de teste disparado!';
+});
